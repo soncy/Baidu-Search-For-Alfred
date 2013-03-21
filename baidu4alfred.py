e020f37484e3f06161c10edbfe0cd3ab6c1570c5
soncy <cs1900@gmail.com>
2013-03-21 19:46:00 +0800

    支持suggest为空时直接搜索
    支持suggest为空时直接搜索

--- baidu4alfred.py
+++ baidu4alfred.py
@@ -2,28 +2,38 @@
 import httplib
 import json, re
 import sys
+import time
 from urllib import quote
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 pattern = re.compile('(.*?)(\[.*?\])(.*?)')
+query = '{query}'.strip().replace('\\', '')
 
 def invoke():
+    now_time = time.time()
     conn = httplib.HTTPConnection("suggestion.baidu.com")
-    conn.request("GET", "/su?wd=" + quote('{query}') +"&p=3&cb=window.bdsug.sug&sid=1427_1945_1788&t=1363856968686")  
+    conn.request("GET", "/su?wd=" + quote(query) +"&t=" + str(int(time.time())))  
     r1 = conn.getresponse() 
     suggest_text = r1.read().decode('gbk') #百度suggest回数为gbk编码
     conn.close()
-    return parse_suggestdata(suggest_text)
+    return join_xml(suggest_text)
 
 def init():
     print invoke()
 
-def parse_suggestdata(text):
+def join_xml(text):
     match = pattern.search(text)
     json_data = json.loads(match.group(2))
 
     result = ['<?xml version="1.0"?>', '<items>']
+
+    if len(json_data) == 0:
+        result.append('<item uid="baidusearch" arg="http://www.baidu.com/s?wd=' + quote(query) + '">')
+        result.append('<title>直接在百度中搜索“' + query + '”</title>')
+        result.append('<subtitle>baidusearch</subtitle>')
+        result.append('<icon>icon.png</icon>')
+        result.append('</item>')
 
     for item in json_data:
         word = quote(item.encode('utf-8'))
@@ -33,6 +43,7 @@ def parse_suggestdata(text):
         result.append('<subtitle>baidu search</subtitle>')
         result.append('<icon>icon.png</icon>')
         result.append('</item>')  
+
     result.append('</items>')
     xml = ''.join(result)    
     return xml
