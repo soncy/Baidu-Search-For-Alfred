ad856be10fb72651fb114e31d0aae2926ebdfe0f
soncy <cs1900@gmail.com>
2013-03-21 22:40:04 +0800

    fix bug
    修复直接输入“baidu 关键词”时排序错乱问题

--- baidu4alfred.py
+++ baidu4alfred.py
@@ -29,14 +29,16 @@ def join_xml(text):
     result = ['<?xml version="1.0"?>', '<items>']
     json_data.insert(0, query)
 
+    i = 0
     for item in json_data:
         word = quote(item.encode('utf-8'))
         url = 'http://www.baidu.com/s?wd=' + word
-        result.append('<item uid="baidusearch" arg="' + url + '">');
+        result.append('<item uid="baidusearch'+str(i)+'" arg="' + url + '">');
         result.append('<title>' + item + '</title>')
         result.append('<subtitle>baidu search</subtitle>')
         result.append('<icon>icon.png</icon>')
         result.append('</item>')  
+        i += 1
 
     result.append('</items>')
     xml = ''.join(result)    
